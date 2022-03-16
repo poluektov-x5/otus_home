a4fff109bf5c4525b72bd712b443977bba24807a
Konstantin Poluektov <Konstantin.Poluektov@x5.ru>
2022-03-16 20:34:11 +0300

    hw02_unpack_string done

--- hw02_unpack_string/go.mod
+++ hw02_unpack_string/go.mod
@@ -1,4 +1,4 @@
-module github.com/fixme_my_friend/hw02_unpack_string
+module github.com/poluektov-x5/otus_home/hw02_unpack_string
 
 go 1.16
 
--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,55 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
+// ErrInvalidString ошибка некорректной строки.
 var ErrInvalidString = errors.New("invalid string")
+var builder = strings.Builder{}
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+// Добавление в итоговую строку символа.
+func add(symbol int32, multiplier int) {
+	builder.WriteString(strings.Repeat(string(symbol), multiplier))
+}
+
+// Сброс итоговой строки.
+func reset() {
+	builder.Reset()
+}
+
+// Unpack - Распаковка строки.
+func Unpack(text string) (string, error) {
+	var symbol int32
+	var multiplier int
+	var operation bool
+
+	reset()
+
+	for index, runeValue := range text {
+		if !unicode.IsDigit(runeValue) {
+			if operation {
+				add(symbol, 1)
+			} else {
+				operation = true
+			}
+			symbol = runeValue
+			if index+1 == len(text) {
+				add(symbol, 1)
+			}
+		} else {
+
+			if operation == false {
+				return "", ErrInvalidString
+			}
+
+			multiplier, _ = strconv.Atoi(string(runeValue))
+			add(symbol, multiplier)
+			operation = false
+		}
+	}
+
+	return builder.String(), nil
 }

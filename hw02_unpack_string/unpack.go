2ca9b02707c7d4f0122cd2bfb458e9e8963d667e
Konstantin Poluektov <Konstantin.Poluektov@x5.ru>
2022-03-22 14:25:17 +0300

    hw02_unpack_string refactoring

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -7,49 +7,83 @@ import (
 	"unicode"
 )
 
-// ErrInvalidString ошибка некорректной строки.
-var ErrInvalidString = errors.New("invalid string")
-var builder = strings.Builder{}
-
-// Добавление в итоговую строку символа.
-func add(symbol int32, multiplier int) {
-	builder.WriteString(strings.Repeat(string(symbol), multiplier))
+// Структура команды.
+type command struct {
+	symbol     int32
+	multiplier int
 }
 
-// Сброс итоговой строки.
-func reset() {
-	builder.Reset()
+// ErrInvalidString ошибка некорректной строки.
+var ErrInvalidString = errors.New("invalid string")
+
+// Обработка команды.
+func process(command command) string {
+	return strings.Repeat(string(command.symbol), command.multiplier)
+}
+
+// Добавление команды в коллекцию.
+func addCommand(list []command, symbol int32, multiplier int) []command {
+	return append(list, command{symbol, multiplier})
+}
+
+// Разбор строки на команды.
+func parseToCommands(text string) ([]command, error) {
+	var list []command
+	var operation bool
+	var symbol int32
+
+	for _, runeValue := range text {
+		isDigit := unicode.IsDigit(runeValue)
+
+		// Если формат строки некорректный.
+		if isDigit && !operation {
+			return list, ErrInvalidString
+		}
+
+		if isDigit {
+			multiplier, _ := strconv.Atoi(string(runeValue))
+			list = addCommand(list, symbol, multiplier)
+			operation = false
+			continue
+		}
+
+		if operation {
+			list = addCommand(list, symbol, 1)
+		} else {
+			operation = true
+		}
+
+		symbol = runeValue
+	}
+
+	// Если последняя операция не завершена.
+	if operation {
+		list = addCommand(list, symbol, 1)
+	}
+
+	return list, nil
+}
+
+// Сборка строки.
+func assemble(list []command) (string, error) {
+	result := strings.Builder{}
+
+	for _, command := range list {
+		result.WriteString(process(command))
+	}
+
+	return result.String(), nil
 }
 
 // Unpack - Распаковка строки.
 func Unpack(text string) (string, error) {
-	var symbol int32
-	var multiplier int
-	var operation bool
-
-	reset()
-
-	for index, runeValue := range text {
-		if !unicode.IsDigit(runeValue) {
-			if operation {
-				add(symbol, 1)
-			} else {
-				operation = true
-			}
-			symbol = runeValue
-			if index+1 == len(text) {
-				add(symbol, 1)
-			}
-		} else {
-			if !operation {
-				return "", ErrInvalidString
-			}
-
-			multiplier, _ = strconv.Atoi(string(runeValue))
-			add(symbol, multiplier)
-			operation = false
-		}
+	// Разбор строки на команды.
+	commands, err := parseToCommands(text)
+	// Если формат строки некорректный.
+	if err != nil {
+		return "", err
 	}
 
-	return builder.String(), nil
+	// Сборка новой строки.
+	return assemble(commands)
 }

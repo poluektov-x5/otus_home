14dcbf5c4401aaf5dfe509e5c737003f54420fd1
Konstantin Poluektov <Konstantin.Poluektov@x5.ru>
2022-04-27 12:04:33 +0300

    hw03_frequency_analysis asterisk

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,7 @@
 package hw03frequencyanalysis
 
 import (
+	"regexp"
 	"sort"
 	"strings"
 )
@@ -8,13 +9,24 @@ import (
 // TopSize - Размер топ-листа.
 const TopSize = 10
 
+var regular = regexp.MustCompile("[^A-Za-zА-Яа-я0-9-]+")
+
 // Top10 - получение списка топ-10.
 func Top10(text string) []string {
+	// Преобразование текста в оригинальный список слов (с повторами).
+	list := convertTextToList(text)
+
+	// Форматирование оригинального списка слов (с повторами).
+	list = formatList(list)
+
+	// Фильтрация оригинального списка слов (с повторами).
+	list = filterList(list)
+
 	// Преобразование текста в карту с количеством повторений.
-	rangeList := convertToRangeMap(text)
+	rangeList := convertToRangeMap(list)
 
 	// Преобразование карты с количеством повторений в список слов (без повторов).
-	list := convertToList(rangeList)
+	list = convertRangeMapToList(rangeList)
 
 	// Сортировка списка по частоте и по алфавиту.
 	sortByRangeByAbc(list, rangeList)
@@ -22,11 +34,38 @@ func Top10(text string) []string {
 	return limit(list, TopSize)
 }
 
-// Преобразование текста в карту с количеством повторений.
-func convertToRangeMap(text string) map[string]int {
-	// Преобразование текста в оригинальный список слов (с повторами).
-	list := strings.Fields(text)
+// Преобразование текста в оригинальный список слов (с повторами).
+func convertTextToList(text string) []string {
+	return strings.Fields(text)
+}
 
+// Форматирование оригинального списка слов (с повторами).
+func formatList(list []string) []string {
+	for index, item := range list {
+		// Преобразование к нижнему регистру.
+		item = strings.ToLower(item)
+
+		// Удаление знаков препинания.
+		list[index] = regular.ReplaceAllString(item, "")
+	}
+
+	return list
+}
+
+// Фильтрация оригинального списка слов (с повторами).
+func filterList(list []string) []string {
+	filteredList := make([]string, 0, len(list))
+	for _, item := range list {
+		if item != "-" {
+			filteredList = append(filteredList, item)
+		}
+	}
+
+	return filteredList
+}
+
+// Преобразование оригинального списка слов в карту с количеством повторений.
+func convertToRangeMap(list []string) map[string]int {
 	// Создание карты слов с количеством повторений.
 	rangeList := make(map[string]int)
 	for _, item := range list {
@@ -37,7 +76,7 @@ func convertToRangeMap(text string) map[string]int {
 }
 
 // Преобразование карты с количеством повторений в список слов (без повторов).
-func convertToList(rangeList map[string]int) []string {
+func convertRangeMapToList(rangeList map[string]int) []string {
 	list := make([]string, 0, len(rangeList))
 	for key := range rangeList {
 		list = append(list, key)
